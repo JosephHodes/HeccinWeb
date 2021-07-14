bb23e72e1302312868cc363412da7fc95a9cda6a
Joseph Hodes <jojoho70@gmail.com>
2021-07-13 21:15:39 -0400

    i decided to use tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import "./Components/Landing"
-import "./App.css"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import Landing from "./Components/Landing/Landing"
+
 function App(): JSX.Element {
   return (
-    <>
-
-    </>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Landing />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/Components/Landing/Landing.tsx
+++ src/Components/Landing/Landing.tsx
@@ -1,7 +1,21 @@
-const landing = (props: object): JSX.Element => {
+import Group from "../../Media/Group.svg"
+import firstwave from "../../Media/firstwave.svg"
+
+
+const Landing = (props: object): JSX.Element => {
+
     return (
         <>
+            <section>
+                <img src={Group} alt="logo" className="-m-1 w-24"/>
+                <div className="text-center text-white">
+                    <div className="text-xl">Howdy, I am</div>
+                    <div className="text-4xl ">Joseph Hodes</div>
+                    <div className="text-xl">I'm a passionate software engineer</div>
+                </div>
+                <img src={firstwave} alt="wave" className="absolute -bottom-20  h-48 w-full md:h-18 md:-bottom-28"/>
+            </section>
         </>
     )
 }
-export default landing;
+export default Landing;
