aa2f008580512cb74c033708080d217772bcb70a
Joseph Hodes <jojoho70@gmail.com>
2021-07-19 23:55:19 -0400

    moved over to angular because preffered framework

---
+++ Server/Main.js
@@ -0,0 +1,11 @@
+const initial = require("express");
+const router = initial();
+const port = 8000;
+const landing = require("")
+router().on("/api", (req , res) =>{
+
+})
+router().listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from 'react-router-dom';
+import Blog from "./Components/Blog/Blog"
+import Group from "./Media/Group.svg";
+import firstwave from "./Media/firstwave.svg";
 
-import Landing from "./Components/Landing/Landing"
 
 function App(): JSX.Element {
   return (
-    <Router>
+      <Router>
+<>
+<section>
+      <img src={Group} alt="logo" className="-m-1 w-24"/>
+    <div className="text-center text-white">
+        <div className="text-xl">Howdy, I am</div>
+        <div className="text-4xl ">Joseph Hodes</div>
+        <div className="text-xl">I'm a passionate software engineer</div>
+    </div>
+    <img src={firstwave} alt="wave" className="absolute -bottom-20  h-48 w-full md:h-18 md:-bottom-28"/>
+
+</section>
+</>
       <Switch>
-        <Route path="/">
-          <Landing />
-        </Route>
+        <Route path={`/blog`}> <Blog></Blog></Route>
       </Switch>
     </Router>
   );
