36b90dfb0e28f47684a1b1ddbabe768fcb3745e4
Joseph Hodes <jojoho70@gmail.com>
2021-08-05 18:48:56 -0400

    added wave animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,12 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    backgroundColor: (theme) => ({
-      ...theme("colors"),
-      background: "#0A192E",
-    }),
-    gradientColorStops: (theme) => ({
-      "h-primary": "#629E92",
-      "h-secondary": "#79DDCA",
-    }),
+    extend: {
+      width: {
+        "75%": "75%",
+      },
+    },
+    s,
   },
   variants: {
     extend: {},
