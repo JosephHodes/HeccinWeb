f9effb7b64d4f62b74c37335330262135ffb1dcc
Joseph Hodes <jojoho70@gmail.com>
2021-08-13 18:00:33 -0400

    added smooth scrolling

--- Angular/src/app/landing/landing.component.ts
+++ Angular/src/app/landing/landing.component.ts
@@ -1,18 +1,36 @@
 import {Component, HostListener, OnInit} from '@angular/core';
 
+
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
-  constructor() {
 
+
+  constructor() {
   }
 
+  @HostListener('window:scroll')
+  checkScroll() {
+
+    // Both window.pageYOffset and document.documentElement.scrollTop returns the same result in all the cases. window.pageYOffset is not supported below IE 9.
+
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    console.log('[scroll]', scrollPosition);
+  }
+
+  scrollToTop = () => {
+    window.scroll({
+      top: -1000,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
 
   ngOnInit(): void {
-
   }
 
 }
--- Angular/tailwind.config.js
+++ Angular/tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
       backgroundColor: {
         blu: "#0099FF",
         secondblu: "#005994",
+        lastblu: "#003C64",
+        bottomBlack: "#00131F"
       },
       spacing: {
         secondWave: "-20%",
@@ -16,6 +18,7 @@ module.exports = {
       height: {
         container: "400vh",
         waves: "200%",
+        littlecontainer: "60vh"
       },
       margin: {
         proper: "3.75rem",
