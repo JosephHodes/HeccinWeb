df22b26276eaa8743326a3906f1538b21f7b0a6f
Joseph Hodes <jojoho70@gmail.com>
2021-08-22 19:46:10 -0400

    testing response

--- Server/Blog/Blog.go
+++ Server/Blog/Blog.go
@@ -1,13 +1,17 @@
 package blog
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 )
 
 func Blog(w http.ResponseWriter, r *http.Request) {
- case r.method
-   "GET" {
-	fmt.Fprintf(w, "hello worlds")
-	log.println("response sent")
+	switch r.method {
+	case "GET":
+		{
+			fmt.Fprintf(w, "hello worlds")
+			log.println("response sent")
+		}
 	}
 }
